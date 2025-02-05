8b8abbb1dbd106135cef0f0202949834f1dd9759
Dheeraj202 <dheerajsadan2002@gmail.com>
2025-02-05 20:10:21 +0530

    add header and footer and creatives

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,5 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link"; // Import Link from next/link
 
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Link href="/editor">
-        <Button>Text Editor</Button>
-      </Link>
-    </div>
-  );
+  return <div className="flex items-center justify-center min-h-screen"></div>;
 }
