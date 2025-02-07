7e722bb6146751a490fe7be77e70beb04f2d0685
Dheeraj202 <dheerajsadan2002@gmail.com>
2025-02-07 15:13:47 +0530

    refractor code and add hover effects to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,60 +3,7 @@ import TraitCard from "@/components/TraitCard";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link"; // Import Link from next/link
-
-const traitRawData = [
-  {
-    imgSvg: "/innovate.svg",
-    heading: "Innovate",
-    subHeading1: "The very first step towards success is innovation. ",
-    subHeading2: "Have any ideas, Let's give it a try?",
-  },
-  {
-    imgSvg: "/build.svg",
-    heading: "Build",
-    subHeading1: "Ideas are necessary but not sufficient.",
-    subHeading2: " Come forward, and start building.",
-  },
-  {
-    imgSvg: "/experimental.svg",
-    heading: "Experiment",
-    subHeading1: "Finish implementing the ideas into a real-life model. ",
-    subHeading2: "Why wait then? Let's go and test it.",
-  },
-];
-
-const HelpRawData = [
-  {
-    img: "/help1.png",
-    heading: "Hands-On Learning 🛠️",
-    subheading:
-      "Get practical experience with robotics, electronics, and programming by working on real-world projects—beyond what’s taught in the classroom.",
-  },
-  {
-    img: "/help2.png",
-    heading: "Mentorship & Guidance 👩‍🏫👨‍🏫",
-    subheading:
-      "Learn from experienced seniors and faculty who provide invaluable support, guidance, and mentorship to help you grow. ",
-  },
-  {
-    img: "/help3.png",
-    heading: "Access to Advanced Tools & Resources ⚙️",
-    subheading:
-      "Work with cutting-edge equipment and technology that will boost your skills and give you an edge in your projects.",
-  },
-  {
-    img: "/help4.png",
-    heading: "Teamwork & Leadership Skills 🤝",
-    subheading:
-      "Collaborate with like-minded students, sharpen your teamwork skills, and even take up leadership roles in various projects.",
-  },
-  {
-    img: "/help5.png",
-    heading: "Boost Your Career 🚀",
-    subheading:
-      " Gain experience that looks great on your resume, preparing you for internships, jobs, and higher studies in robotics, AI, or any tech-related field.",
-  },
-];
+import { traitRawData, HelpRawData } from "../raw data/indexPage";
 
 export default function Home() {
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,7 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const links: { name: string; href: string }[] = [
   { name: "home", href: "/" },
@@ -8,15 +11,30 @@ const links: { name: string; href: string }[] = [
   { name: "login", href: "/login" },
 ];
 export default function Header() {
+  const router = useRouter();
   return (
     <div className="top-0 flex flex-row items-center justify-between w-full text-white font-alata px-10 pt-5 ">
-      <p className="text-[32px] ">
+      {/* <p className="text-[32px] ">
         Robotics <b className="text-[#6157C9]">PEC</b>
-      </p>
+      </p> */}
+      <Image
+        src="/logo.png"
+        width={200}
+        height={100}
+        alt=""
+        className="p-0 hover:cursor-pointer"
+        onClick={() => {
+          router.replace("/");
+        }}
+      />
       <div className="flex flex-row items-center gap-28  font-[600] ">
         {links.map((link: { name: string; href: string }, index) => {
           return (
-            <a href={link.href} key={index} className="text-xl">
+            <a
+              href={link.href}
+              key={index}
+              className="text-xl relative pb-1 after:absolute after:inset-x-0 after:bottom-0 after:h-[2px] after:bg-current after:scale-x-0 after:transition hover:after:scale-x-100"
+            >
               {link.name}
             </a>
           );
